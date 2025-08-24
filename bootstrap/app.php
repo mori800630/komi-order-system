a00e4d055cbe6310f0d29082baf41b7b6b20a572
mori800630 <mori800630@gmail.com>
2025-08-24 18:46:17 +0900

    Remove ForceHttps middleware and use URL::forceScheme instead to prevent redirect loops

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -14,9 +14,6 @@ return Application::configure(basePath: dirname(__DIR__))
         $middleware->alias([
             'role' => \App\Http\Middleware\CheckRole::class,
         ]);
-        
-        // HTTPS強制ミドルウェアを追加
-        $middleware->append(\App\Http\Middleware\ForceHttps::class);
     })
     ->withExceptions(function (Exceptions $exceptions): void {
         //
