923ac45fe4993809aac83ecf5b601c7f74b5b5b3
mori800630 <mori800630@gmail.com>
2025-08-24 20:31:26 +0900

    Add OrderStatusTransitionSeeder to DatabaseSeeder to fix missing status transitions

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -16,6 +16,7 @@ class DatabaseSeeder extends Seeder
         $this->call([
             DepartmentSeeder::class,
             OrderStatusSeeder::class,
+            OrderStatusTransitionSeeder::class,
             ProductSeeder::class,
             UserSeeder::class,
         ]);
